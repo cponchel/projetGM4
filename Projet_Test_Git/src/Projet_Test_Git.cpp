fa52fa8013022bdb3947fc5f72cd4d9a0a26bf17
cponchel <colette.ponchel@insa-rouen.fr>
2017-03-17 11:52:20 +0100

    Ajout de "wesh alors"

--- Projet_Test_Git/src/Projet_Test_Git.cpp
+++ Projet_Test_Git/src/Projet_Test_Git.cpp
@@ -11,6 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "Première modification, inspi de ouf" << endl;
+	cout << "Première modification, inspi de ouf wesh alors" << endl;
 	return 0;
 }
