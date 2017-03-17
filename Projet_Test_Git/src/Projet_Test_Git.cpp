f3e6331fd0e67fc1497db685757c8874c1967f31
cponchel <colette.ponchel@insa-rouen.fr>
2017-03-17 11:51:13 +0100

    Ajout d'une ligne d'affichage dans le main

---
+++ Projet_Test_Git/src/Projet_Test_Git.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Projet_Test_Git.cpp
+// Author      : Colette Ponchel
+// Version     :
+// Copyright   : C'est à moi
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Première modification, inspi de ouf" << endl;
+	return 0;
+}
